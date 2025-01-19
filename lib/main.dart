10ee80bbe597923389fbd72c2cf6cc86f33bed26
Nattapong Tonprasert <nattapong.ton@ttbbank.com>
2025-01-19 17:20:59 +0700

    Integrate GetIt for dependency injection and update main application structure
    - Added `get_it` package as a direct dependency in `pubspec.yaml` for dependency injection.
    - Updated `pubspec.lock` to include the new `get_it` version 8.0.3.
    - Refactored `main.dart` to initialize dependency injection before running the app.
    - Created `injection_container.dart` to configure and register dependencies for authentication services.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:tdd_tutorial/core/services/injection_container.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await init();
+  runApp(const MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
